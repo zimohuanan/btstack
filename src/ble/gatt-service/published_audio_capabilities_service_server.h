038ced5448cd4b61d46e59eda9532528d59f5159
Milanka Ringwald <mila@ringwald.ch>
2022-01-28 15:17:02 +0100

    gatt-service/ascs,bass,pacs: add skeleton files

---
+++ src/ble/gatt-service/published_audio_capabilities_service_server.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2022 BlueKitchen GmbH
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ * 4. Any redistribution, use, or modification is done solely for
+ *    personal benefit and not for any commercial purpose or for
+ *    monetary gain.
+ *
+ * THIS SOFTWARE IS PROVIDED BY BLUEKITCHEN GMBH AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BLUEKITCHEN
+ * GMBH OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * Please inquire about commercial licensing options at 
+ * contact@bluekitchen-gmbh.com
+ *
+ */
+
+/**
+ * @title Published Audio Capabilities Service Server (PACS)
+ * 
+ */
+
+#ifndef PUBLISHED_AUDIO_CAPABILITIES_SERVICE_SERVER_H
+#define PUBLISHED_AUDIO_CAPABILITIES_SERVICE_SERVER_H
+
+#include <stdint.h>
+
+#if defined __cplusplus
+extern "C" {
+#endif
+
+/* API_START */
+
+/**
+ * @text The Published Audio Capabilities Service Server exposes server audio capabilities and audio
+ * availability. 
+ * 
+ * To use with your application, add `#import <published_audio_capabilities_service.gatt>` to your .gatt file. 
+ */
+
+/**
+ * @brief Init Published Audio Capabilities Service Server with ATT DB
+ */
+void published_audio_capabilities_service_server_init(void);
+
+/* API_END */
+
+#if defined __cplusplus
+}
+#endif
+
+#endif
+
