71ecd678a4ea8b678e1661838cfb4aa53c6f9a2c
Milanka Ringwald <mila@ringwald.ch>
2022-03-01 15:03:08 +0100

    test/gatt_service/bass: test events and state for scan start/stop commands

--- test/gatt_service/broadcast_audio_scan_service_test.cpp
+++ test/gatt_service/broadcast_audio_scan_service_test.cpp
@@ -28,6 +28,9 @@
 static bass_source_t sources[2];
 static uint8_t sources_num = 2;
 
+static uint8_t expected_scan_active = 0;
+static uint8_t expected_event = 0;
+
 TEST_GROUP(BROADCAST_AUDIO_SCAN_SERVICE_SERVER){ 
     att_service_handler_t * service; 
     uint16_t con_handle;
@@ -112,7 +115,6 @@ TEST(BROADCAST_AUDIO_SCAN_SERVICE_SERVER, write_receive_state_client_configurati
     }
 }
 
-
 TEST(BROADCAST_AUDIO_SCAN_SERVICE_SERVER, read_receive_state_source_id){
     uint8_t expected_bass_source[15];
     uint8_t read_buffer[50];
@@ -129,6 +131,92 @@ TEST(BROADCAST_AUDIO_SCAN_SERVICE_SERVER, read_receive_state_source_id){
         memset(expected_bass_source, 0, sizeof(expected_bass_source));
         expected_bass_source[0] = item->source_id;
         MEMCMP_EQUAL(expected_bass_source, read_buffer, sizeof(expected_bass_source));
+    }
+}
+
+static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size){
+    UNUSED(channel);
+    UNUSED(size);
+
+    CHECK_EQUAL(packet_type, HCI_EVENT_PACKET);
+    if (packet[0] != HCI_EVENT_GATTSERVICE_META){
+        return;
+    }
+
+    if (expected_event != 0){
+        CHECK_EQUAL(expected_event, packet[2]); 
+    }
+
+    switch (packet[2]){
+    
+        case GATTSERVICE_SUBEVENT_BASS_REMOTE_SCAN_STOPED:
+            CHECK_EQUAL(expected_scan_active, 0); 
+            break;
+        
+        case GATTSERVICE_SUBEVENT_BASS_REMOTE_SCAN_STARTED:
+            CHECK_EQUAL(expected_scan_active, 1); 
+            break;
+
+        default:
+            break;
+    }
+
+}
+
+TEST(BROADCAST_AUDIO_SCAN_SERVICE_SERVER, write_receive_state_source_id){
+    uint8_t expected_bass_source[15];
+    uint8_t read_buffer[50];
+    uint8_t write_buffer[10];
+
+    btstack_linked_list_iterator_t it;    
+    btstack_linked_list_iterator_init(&it, bass_sources);
+    
+    bass_source_t * item = (bass_source_t*) btstack_linked_list_iterator_next(&it);
+    
+    broadcast_audio_scan_service_server_register_packet_handler(&packet_handler);
+
+    // empty buffer
+    uint16_t write_buffer_size = 0;
+    int response = mock_att_service_write_callback(con_handle, bass_audio_scan_control_point_handle, ATT_TRANSACTION_MODE_NONE, 0, write_buffer, write_buffer_size);
+    CHECK_EQUAL(BASS_ERROR_CODE_OPCODE_NOT_SUPPORTED, response); 
+
+    uint8_t opcode;
+    for (opcode = (uint8_t)LEA_BASS_OPCODE_REMOTE_SCAN_STOPPED; opcode <= (uint8_t)LEA_BASS_OPCODE_RFU; opcode++){
+        int expected_response = 0;
+        write_buffer_size = 1;
+        expected_event = 0;
+        
+        switch ((lea_bass_opcode_t)opcode){
+            case LEA_BASS_OPCODE_REMOTE_SCAN_STOPPED:
+                expected_scan_active = 0;
+                expected_event = GATTSERVICE_SUBEVENT_BASS_REMOTE_SCAN_STOPED;
+                break;
+
+            case LEA_BASS_OPCODE_REMOTE_SCAN_STARTED:
+                expected_scan_active = 1;
+                expected_event = GATTSERVICE_SUBEVENT_BASS_REMOTE_SCAN_STARTED;
+                break;
+
+            case LEA_BASS_OPCODE_ADD_SOURCE:
+                break;
+
+            case LEA_BASS_OPCODE_MODIFY_SOURCE:
+                break;
+
+            case LEA_BASS_OPCODE_SET_BROADCAST_CODE:
+                break;
+
+            case LEA_BASS_OPCODE_REMOVE_SOURCE:
+                break;
+
+            default:
+                expected_response = BASS_ERROR_CODE_OPCODE_NOT_SUPPORTED;
+                break;
+        }   
+
+        write_buffer[0] = opcode;
+        response = mock_att_service_write_callback(con_handle, bass_audio_scan_control_point_handle, ATT_TRANSACTION_MODE_NONE, 0, write_buffer, write_buffer_size);
+        CHECK_EQUAL(expected_response, response); 
     }
 }
 
