5b13088790cddc90eb07b9bb306954e032a164d7
Matthias Ringwald <matthias@ringwald.ch>
2022-04-06 22:13:01 +0200

    example: add pbap_server_demo

---
+++ example/pbap_server_demo.c
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2022 BlueKitchen GmbH
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ * 4. Any redistribution, use, or modification is done solely for
+ *    personal benefit and not for any commercial purpose or for
+ *    monetary gain.
+ *
+ * THIS SOFTWARE IS PROVIDED BY BLUEKITCHEN GMBH AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BLUEKITCHEN
+ * GMBH OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * Please inquire about commercial licensing options at 
+ * contact@bluekitchen-gmbh.com
+ *
+ */
+
+#define BTSTACK_FILE__ "pbap_client_demo.c"
+
+// *****************************************************************************
+/* EXAMPLE_START(pbap_server_demo): PBAP Server - Demo Phonebook Server
+ */
+// *****************************************************************************
+
+
+#define PBAP_SERVER_L2CAP_PSM         0x1001
+#define PBAP_SERVER_RFCOMM_CHANNEL_NR 1
+
+#include "btstack_config.h"
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "btstack.h"
+
+static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size);
+
+static btstack_packet_callback_registration_t hci_event_callback_registration;
+
+static uint16_t pbap_cid;
+
+static uint8_t service_buffer[150];
+
+// packet handler
+static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size){
+    UNUSED(channel);
+    UNUSED(size);
+    int i;
+    uint8_t status;
+    switch (packet_type){
+        case HCI_EVENT_PACKET:
+            switch (hci_event_packet_get_type(packet)) {
+                case HCI_EVENT_PBAP_META:
+                    switch (hci_event_pbap_meta_get_subevent_code(packet)){
+                        case PBAP_SUBEVENT_CONNECTION_OPENED:
+                            status = pbap_subevent_connection_opened_get_status(packet);
+                            if (status){
+                                printf("[!] Connection failed, status 0x%02x\n", status);
+                            } else {
+                                pbap_cid = pbap_subevent_connection_opened_get_pbap_cid(packet);
+                                printf("[+] Connected pbap_cid 0x%04x\n", pbap_cid);
+                            }
+                            break;
+                        case PBAP_SUBEVENT_CONNECTION_CLOSED:
+                            printf("[+] Connection closed\n");
+                            break;
+                        case PBAP_SUBEVENT_OPERATION_COMPLETED:
+                            printf("[+] Operation complete, status 0x%02x\n",
+                                   pbap_subevent_operation_completed_get_status(packet));
+                            break;
+                        case PBAP_SUBEVENT_AUTHENTICATION_REQUEST:
+                            printf("[?] Authentication requested\n");
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case PBAP_DATA_PACKET:
+            for (i=0;i<size;i++){
+                printf("%c", packet[i]);
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+int btstack_main(int argc, const char * argv[]);
+int btstack_main(int argc, const char * argv[]){
+    (void)argc;
+    (void)argv;
+
+    // init L2CAP
+    l2cap_init();
+
+#ifdef ENABLE_BLE
+    // Initialize LE Security Manager. Needed for cross-transport key derivation
+    sm_init();
+#endif
+
+    // init RFCOM
+    rfcomm_init();
+
+    // init GOEP Server
+    goep_server_init();
+
+    // init PBAP Server
+    pbap_server_init(&packet_handler, PBAP_SERVER_RFCOMM_CHANNEL_NR, PBAP_SERVER_L2CAP_PSM, LEVEL_2);
+
+    // setup SDP Record
+    sdp_init();
+    pbap_server_create_sdp_record(service_buffer, sdp_create_service_record_handle(), PBAP_SERVER_RFCOMM_CHANNEL_NR,
+                                  PBAP_SERVER_L2CAP_PSM, "Phonebook Access PSE", 1, 3);
+    sdp_register_service(service_buffer);
+
+    // register for HCI events
+    hci_event_callback_registration.callback = &packet_handler;
+    hci_add_event_handler(&hci_event_callback_registration);
+
+    // turn on!
+    hci_power_control(HCI_POWER_ON);
+
+    return 0;
+}
+/* EXAMPLE_END */
